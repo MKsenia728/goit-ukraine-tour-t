cf3b234f9f30a333e18b768cfc20be1633f5dccf
Dima Hirchyk <dimzuk04@gmail.com>
2024-11-09 15:50:09 +0200

    3

---
+++ src/rewiew.js
@@ -0,0 +1,13 @@
+const scrollBtns = document.querySelectorAll('.scroll-btn');
+const blockComment = document.querySelector('.block-comment');
+const scrollAmount = 376 + 16; // ширина елемента + gap
+
+scrollBtns.forEach(button => {
+  button.addEventListener('click', () => {
+    const direction = button.getAttribute('data-direction');
+    blockComment.scrollBy({
+      left: direction === 'right' ? scrollAmount : -scrollAmount,
+      behavior: 'smooth',
+    });
+  });
+});
